3e564bd0352a9ee1d080618d238b3de10ef5fef8
Peter <petercw94@gmail.com>
2024-01-11 21:11:42 -0700

    removed files no longer needed due to the re-write

--- include/utils.h
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
-
-void appendToString(char* s, char c, int index);
-void resetStringArray(char* s);
-void appendToStringArray(char*array[], char c, int index);
-
